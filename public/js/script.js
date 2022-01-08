b6ae98ebceb472e56204a74203e0d954a30b85b2
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-07 17:29:29 -0700

    Created object that loops through fetch, only want 4

--- public/js/script.js
+++ public/js/script.js
@@ -11,20 +11,31 @@ getNASA();
 //creating fetch and converting data to json
 function getNASA(){
     fetch(baseURL)
-    .then(res =>res.json)
+    .then(res =>res.json())
     .then(data => {
-        console.log(data)
+        const {
+            ...nasaData //use spread operator to later map
+        } = data;
+        // console.log(nasaData); //works
+Object.entries(nasaData).map(([key, data]) =>
+        {
+            return {
+                date: data.date,
+                content: data.explanation,
+                title: data.title,
+                url: data.url
+            }
+        }) //loop through an object
+
     })
 }
 
 
 
-// {
-//     "date": "2004-07-05",
-//     "explanation": "What causes the patterns in Saturn's rings?  The Cassini spacecraft just entering orbit around Saturn has started sending back spectacular images of Saturn's immense ring system in unprecedented detail.  The physical cause for many of newly resolved ring structures is not always understood.   The cause for the beautifully geometric type of ring structure shown above in Saturn's A ring, however, is hypothesized to be a spiral density wave.  A small moon systematically perturbing the orbits of ring particles orbiting at slightly different distances causes such a density wave bunching.  Also visible on the image right is a bending wave, a vertical wave in ring particles also caused by the gravity of a nearby moon.  This close-up spans about 220 kilometers. Cassini is scheduled to take and send back images of the distant ringed Saturn and its unusual moons for the next four years.",
-//     "hdurl": "https://apod.nasa.gov/apod/image/0407/ringwaves_cassini_big.jpg",
-//     "media_type": "image",
-//     "service_version": "v1",
-//     "title": "Cassini Images Density Waves in Saturn's Rings",
-//     "url": "https://apod.nasa.gov/apod/image/0407/ringwaves_cassini.jpg"
-//     },
+// date: "2021-04-04"
+// explanation: "Four moons are visible on the featured image -- can you find them all? First -- and farthest in the background -- is Titan, the largest moon of Saturn and one of the larger moons in the Solar System.  The dark feature across the top of this perpetually cloudy world is the north polar hood. The next most obvious moon is bright Dione, visible in the foreground, complete with craters and long ice cliffs. Jutting in from the left are several of Saturn's expansive rings, including Saturn's A ring featuring the dark Encke Gap. On the far right, just outside the rings, is Pandora, a moon only 80-kilometers across that helps shepherd Saturn's F ring. The fourth moon?  If you look closely inside Saturn's rings, in the Encke Gap, you will find a speck that is actually Pan. Although one of Saturn's smallest moons at 35-kilometers across, Pan is massive enough to help keep the Encke gap relatively free of ring particles. After more than a decade of exploration and discovery, the Cassini spacecraft ran low on fuel in 2017 and was directed to enter Saturn's atmosphere, where it surely melted."
+// hdurl: "https://apod.nasa.gov/apod/image/2104/fourmoons_cassini_960.jpg"
+// media_type: "image"
+// service_version: "v1"
+// title: "In, Through, and Beyond Saturn's Rings"
+// url: "https://apod.nasa.gov/apod/image/2104/fourmoons_cassini_960.jpg"

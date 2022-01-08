f1fdaf0070b3a767bb95273bbec9ac6ba4a116be
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-08 05:42:47 -0700

    formatted date to preferred style

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,12 @@
-console.log("Support Laurel launch her dream career... 🚀")
+console.log("Support Laurel launch her dream career... 🚀");
 
 // const apiKEY = "IIynkiUi3iVEppyNc1Q0pAEfUURrR0FGy9T7OGaE";
-
 const baseURL = `https://api.nasa.gov/planetary/apod?api_key=IIynkiUi3iVEppyNc1Q0pAEfUURrR0FGy9T7OGaE&count=4`;
 const imageCards = document.querySelector('[data-nasa-card]');
 let nasaData;
-
 // console.log(baseURL); //works
 
 getNASA();
-
 //creating fetch and converting data to json
 function getNASA(){
     fetch(baseURL)
@@ -22,8 +19,7 @@ function getNASA(){
         // nasaData); //works
         displayNasaCards(nasaData);
     })
-
-}
+};
 
 //rendering cards on page
 function displayNasaCards(cardData){
@@ -31,20 +27,17 @@ function displayNasaCards(cardData){
 // cardData) //works
 
 // console.log(cardData.length) //undefined
-
 const nasaArray = Object.getOwnPropertyNames(cardData);
 
 for (let i = 0; i < nasaArray.length; i++) {
-console.log("loop check");
-
+// console.log("loop check"); //works
     imageCards.innerHTML +=     
-    `<div class="col-lg-6 mb-4">
-
+    `<div class="col-lg-6 mb-3">
     <div class="card">
         <img class="card-img-top" src="${cardData[i].url}" alt="${cardData[i].title}">
     
         <div class="card-body">
-            <h5 class="card-title">${cardData[i].title} || ${cardData[i].date} <a href="${cardData[i].url}" class="btn btn-outline-secondary btn-sm">
+            <h5 class="card-title">${cardData[i].title} || ${formatDate(new Date(cardData[i].date))} <a href="${cardData[i].url}" class="btn btn-outline-secondary btn-sm">
             <i class="far fa-heart"></i></a></h5>
             <p>${cardData[i].explanation}</p>
             <a href="${cardData[i].url}" class="btn btn-outline-primary btn-block">
@@ -54,12 +47,19 @@ console.log("loop check");
     </div>
     </div>`
 }
-
-
 };
 
+function formatDate(date){
+        return date.toLocaleDateString(
+              undefined,
+              {day: 'numeric', month: 'long', year: 'numeric'}
+        );
+      };
+      
+      console.log(formatDate(new Date()));
+
 //TODO: Format Date
-//TODO: Format cards
+//TODO: Format cards - two cards per row as default, break after two cards - can i do this within one loop?
 //TODO: Create expand feature for explanation
 //TODO: Create link on img to make image larger
 

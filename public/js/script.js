d4f7f8cc9cc7ab50ce86b266bb1f91b22e32407c
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-07 17:52:16 -0700

    Successfully set nasaData globally

--- public/js/script.js
+++ public/js/script.js
@@ -3,8 +3,10 @@ console.log("Coming soon... 🚀")
 // const apiKEY = "IIynkiUi3iVEppyNc1Q0pAEfUURrR0FGy9T7OGaE";
 
 const baseURL = `https://api.nasa.gov/planetary/apod?api_key=IIynkiUi3iVEppyNc1Q0pAEfUURrR0FGy9T7OGaE&count=4`;
+const imageCards = document.querySelector('[data-nasa-card]');
+let nasaData;
 
-console.log(baseURL);
+// console.log(baseURL); //works
 
 getNASA();
 
@@ -16,20 +18,51 @@ function getNASA(){
         const {
             ...nasaData //use spread operator to later map
         } = data;
-        // console.log(nasaData); //works
-Object.entries(nasaData).map(([key, data]) =>
+        console.log("Test 1",
+        nasaData); //works
+Object.entries(nasaData).map(([key, data]) => //do i need the key?
         {
             return {
+                key,
                 date: data.date,
                 content: data.explanation,
                 title: data.title,
                 url: data.url
             }
         }) //loop through an object
-
+        displayNasaCards(nasaData);
     })
+
 }
 
+// displayNasaCards();
+//rendering cards on page
+function displayNasaCards(cardData){
+console.log("Test 2",
+cardData)
+// array.forEach(nasaData, i => {
+//     imageCards.innerHTML =     `<div class="col-lg-6 mb-4">
+
+//     <div class="card">
+//         <img class="card-img-top" src="${[i].url}" alt="${[i].title}">
+    
+//         <div class="card-body">
+//             <h5 class="card-title">${[i].title} || ${[i].date} <a href="${[i].url}" class="btn btn-outline-secondary btn-sm">
+//             <i class="far fa-heart"></i></a></h5>
+//             <p>${[i].content}</p>
+//             <a href="${[i].url}" class="btn btn-outline-primary btn-block">
+//                 Learn More...
+//             </a>
+//         </div>
+//     </div>
+//     </div>`
+
+// });
+
+
+};
+
+//TODO: Format Date
 
 
 // date: "2021-04-04"

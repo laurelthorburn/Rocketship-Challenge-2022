ee45f916ce56a3f233ea9dacd886ba5674bab754
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-09 15:43:34 -0700

    Styling loading page

--- public/js/script.js
+++ public/js/script.js
@@ -134,6 +134,8 @@ function reloadBtn(){
   renderBtn.addEventListener('click', reloadPage);
 
   window.addEventListener("load", removeLoader);
+
+  
 //TODO: Format cards - two cards per row as default, break after two cards - can i do this within one loop?
 //TODO: CSS Styling (h4, explanation, button
 // TODO: aboutLaurel page with back to main page button
